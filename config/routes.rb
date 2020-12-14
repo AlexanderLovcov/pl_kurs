0face72708a46e3dc71eb954d71506f1dabcaa2f
Anendo <sasha.lovcov.98@gmail.com>
2020-12-15 00:53:33 +0300

    qqw

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' }
 
   root 'home#index'
-  resources :tours
+  resources :tours do
+    get :filter
+  end
   resources :home, only: :index
   resources :countries
 end
